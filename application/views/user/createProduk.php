fde00d0b50ea26f657c33bea0811fd3c63061a7d
Aditya Septa <lookadityasepta@gmail.com>
2018-03-27 19:46:28 +0700

    PO Tempahan

--- application/views/user/createProduk.php
+++ application/views/user/createProduk.php
@@ -128,6 +128,15 @@
                                                         <option value="Trading" <?php $a= set_value('kategori'); if($a=="Trading"){?> selected="" <?php } ?>>Trading</option>
                                                     </select>
                                                 </div>
+                                                <label class="col-sm-2 control-label">Satuan</label>
+                                                <div class="col-md-2">
+                                                    <select name="satuan" required="" class="form-control">
+                                                      <option value="Pcs" <?php $a= set_value('satuan'); if($a=="Pcs"){?> selected="" <?php } ?>>Pcs</option>
+                                                      <option value="Liter" <?php $a= set_value('satuan'); if($a=="Liter"){?> selected="" <?php } ?>>Liter</option>
+                                                      <option value="Gram" <?php $a= set_value('satuan'); if($a=="Gram"){?> selected="" <?php } ?>>Gram</option>
+                                                      <option value="Carat" <?php $a= set_value('satuan'); if($a=="Carat"){?> selected="" <?php } ?>>Carat</option>
+                                                    </select>
+                                                </div>
                                                 <label class="col-sm-2 control-label">Status Katalog</label>
                                                 <div class="col-sm-1" style="margin-top: 7px;">
                                                     <div class="i-checks"><label> <input type="radio" <?php $a= set_value('statusKatalog'); if($a=="Tampil"){?> checked="" <?php } ?> value="Tampil" name="statusKatalog" required> <i></i> Tampil </label></div>
--- produk.sql
+++ produk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2018 at 02:06 PM
+-- Generation Time: Mar 27, 2018 at 02:46 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -33,6 +33,7 @@ CREATE TABLE `produk` (
   `jenisProduk` varchar(100) NOT NULL,
   `kategori` varchar(200) NOT NULL,
   `deskripsi` text NOT NULL,
+  `satuan` varchar(200) NOT NULL,
   `hargaModal` int(11) NOT NULL,
   `hargaJual` int(11) NOT NULL,
   `kodeGambar` varchar(255) NOT NULL,
@@ -49,14 +50,14 @@ CREATE TABLE `produk` (
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`idProduk`, `kodeProduk`, `namaProduk`, `jenisProduk`, `kategori`, `deskripsi`, `hargaModal`, `hargaJual`, `kodeGambar`, `gambar1`, `gambar2`, `gambar3`, `gambar4`, `stok`, `statusKatalog`, `lastModified`) VALUES
-(1, 'CLS-100', 'Muhammad Andre-1-Ruby', 'Cincin', 'Tempahan', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 1200000, 1545000, 'IQjrAaLKJe', '', '', '', '', 1, 'Tampil', '2018-03-27 07:41:54'),
-(2, 'CLC-121', 'Denanda Putri-2-Saphire', 'Cincin', 'Massal', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 980000, 1312000, 'W7G84wHaba', '', '', '', '', 0, 'Tidak Tampil', '2018-03-27 07:42:00'),
-(3, 'CLB-12', 'ko joni-3-peridot', 'Cincin', 'Trading', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 560000, 590000, '9vkvHGWtR5', '', '', '', '', 0, 'Tampil', '2018-03-27 07:44:31'),
-(6, 'KMP-18', 'Denanda Putri-3-Asghard', 'Cincin', 'Katalog', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 280000, 320000, 'wDrLsAfUXD', '', '', '', '', 0, 'Tampil', '2018-03-27 07:42:11'),
-(7, 'KRT-1', 'Ferdinan Sinaga-4-8', 'Cincin', 'Massal', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 134000, 185000, 'SI7Wt9PZiW', '', '', '', '', 0, 'Tampil', '2018-03-27 07:44:25'),
-(9, 'CLB01', 'Cincin Perak Berongga', 'Cincin', 'Katalog', 'Bahan: Perak\r\nBatu Permata: Saphire Biru\r\nUkuran: 18', 280000, 0, 'spYGKGHsda', 'spYGKGHsda1.jpg', 'spYGKGHsda2.jpg', NULL, NULL, 0, 'Tampil', '2018-03-27 11:50:23'),
-(10, 'CLB02', 'Cincin Saphire Biru Doff', 'Cincin', 'Katalog', 'Cincin Emas', 450000, 530000, 'YTlJ04wNwZ', 'YTlJ04wNwZ1.jpg', 'YTlJ04wNwZ2.jpg', NULL, NULL, 0, 'Tampil', '2018-03-27 11:52:49');
+INSERT INTO `produk` (`idProduk`, `kodeProduk`, `namaProduk`, `jenisProduk`, `kategori`, `deskripsi`, `satuan`, `hargaModal`, `hargaJual`, `kodeGambar`, `gambar1`, `gambar2`, `gambar3`, `gambar4`, `stok`, `statusKatalog`, `lastModified`) VALUES
+(1, 'CLS-100', 'Muhammad Andre-1-Ruby', 'Cincin', 'Tempahan', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 'Pcs', 1200000, 1545000, 'IQjrAaLKJe', '', '', '', '', 1, 'Tampil', '2018-03-27 12:45:50'),
+(2, 'CLC-121', 'Denanda Putri-2-Saphire', 'Cincin', 'Massal', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 'Pcs', 980000, 1312000, 'W7G84wHaba', '', '', '', '', 0, 'Tidak Tampil', '2018-03-27 12:45:57'),
+(3, 'CLB-12', 'ko joni-3-peridot', 'Cincin', 'Trading', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 'Pcs', 560000, 590000, '9vkvHGWtR5', '', '', '', '', 0, 'Tampil', '2018-03-27 12:45:59'),
+(6, 'KMP-18', 'Denanda Putri-3-Asghard', 'Cincin', 'Katalog', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 'Pcs', 280000, 320000, 'wDrLsAfUXD', '', '', '', '', 0, 'Tampil', '2018-03-27 12:46:01'),
+(7, 'KRT-1', 'Ferdinan Sinaga-4-8', 'Cincin', 'Massal', 'It is a long established fact that a reader will be distracted by the readable                                         content of a page when looking at its layout. The point of using Lorem Ipsum is                                         that it has a more-or-less normal distribution of letters, as opposed to using                                         ''Content here, content here'', making it look like readable English.', 'Pcs', 134000, 185000, 'SI7Wt9PZiW', '', '', '', '', 0, 'Tampil', '2018-03-27 12:46:03'),
+(9, 'CLB01', 'Cincin Perak Berongga', 'Cincin', 'Katalog', 'Bahan: Perak\r\nBatu Permata: Saphire Biru\r\nUkuran: 18', 'Pcs', 280000, 0, 'spYGKGHsda', 'spYGKGHsda1.jpg', 'spYGKGHsda2.jpg', NULL, NULL, 0, 'Tampil', '2018-03-27 12:46:04'),
+(10, 'CLB02', 'Cincin Saphire Biru Doff', 'Cincin', 'Katalog', 'Cincin Emas', 'Pcs', 450000, 530000, 'YTlJ04wNwZ', 'YTlJ04wNwZ1.jpg', 'YTlJ04wNwZ2.jpg', NULL, NULL, 0, 'Tampil', '2018-03-27 12:46:06');
 
 --
 -- Indexes for dumped tables
