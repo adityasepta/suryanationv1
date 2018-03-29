8a6094ffdd8ce21c3b548531da2164492691e666
Aditya Septa <lookadityasepta@gmail.com>
2018-03-29 16:32:54 +0700

    Pergerakan Barang

--- application/views/user/stokBarang.php
+++ application/views/user/stokBarang.php
@@ -39,16 +39,6 @@
         <div class="navbar-header">
             <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
         </div>
-            <!-- <ul class="nav navbar-top-links navbar-right">
-                <li>
-                    <span class="m-r-sm text-muted welcome-message">Selamat Datang Victoriavici.</span>
-                </li>
-                <li>
-                    <a href="login.html">
-                        <i class="fa fa-sign-out"></i> Log out
-                    </a>
-                </li>
-            </ul> -->
 
         </nav>
         </div>
@@ -75,8 +65,11 @@
                         <div class="ibox-title">
                             <h5>Stok Barang</h5>
                             <div class="ibox-tools">
-                                <a class="btn btn-xs btn-primary" href="<?php echo base_url();?>user/createInventory">
+                                <!-- <a class="btn btn-xs btn-primary" href="<?php echo base_url();?>user/createInventory">
                                     <i class="fa fa-pencil"><span style="font-family: 'open sans'"><strong> TAMBAH STOK BARANG</strong></span></i>
+                                </a> -->
+                                <a class="btn btn-xs btn-primary" data-toggle="modal" data-target="#modal">
+                                    <i class="fa fa-pencil"></i><strong> TAMBAH PERGERAKAN</strong>
                                 </a>
                             </div>
                         </div>
@@ -108,7 +101,7 @@
                                         <td><?php echo $p->kodeProduk?></td>
                                         <td><?php echo $p->namaProduk?></td>
                                         <td><?php echo $p->jenisPergerakanBarang?></td>
-                                        <td><?php echo $p->jumlah?></td></td>
+                                        <td><?php echo $p->jumlah?></td>
                                         <!-- <td>Rp. <?php echo number_format($p->hargaBeli,2,".","."); ?> -->
                                         <td><!-- Button trigger modal -->
                                                         <!-- <a href="<?php echo base_url()?>user/editInventory/<?php echo $p->idStok?>" class="btn btn-xs btn-default">Edit</a> -->
@@ -124,6 +117,71 @@
                                 </tr>
                                 </tfoot>
                             </table>
+                            <!-- Modal -->
+                            <div class="modal inmodal fade" id="modal" tabindex="-1" role="dialog"  aria-hidden="true">
+                                <div class="modal-dialog modal-sm">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                            <h3 class="modal-title">Jenis Produksi</h3>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <?php echo form_open_multipart('user/tambahRekapProduksi','class="form-horizontal"')?>
+                                                    <h3 class="m-t-none m-b">Pilih Jenis Produksi</h3>
+                                                    <select id="jenisProduksi" class="form-control"  name="jenisProduksi">
+                                                        <option value="tempahan">Tempahan</option>
+                                                        <option value="massal">Massal</option>
+                                                        <option value="perak">Perak</option>
+                                                    </select>
+                                                    <hr>
+                                                    <h3 class="m-t-none m-b">Pilih Pegawai</h3>
+                                                    <div id="idUser" class="selectpicker" data-live="true">
+                                                        <button data-id="prov" type="button" class="btn btn-lg btn-block btn-default dropdown-toggle">
+                                                            <span class="placeholder">Pilih Pegawai</span>
+                                                            <span class="caret"></span>
+                                                        </button>
+                                                        <div class="dropdown-menu">
+                                                            <div class="live-filtering" data-clear="true" data-autocomplete="true" data-keys="true">
+                                                                <label class="sr-only" for="input-bts-ex-4">Search in the list</label>
+                                                                <div class="search-box">
+                                                                    <div class="input-group">
+                                                                        <span class="input-group-addon" id="search-icon3">
+                                                                            <span class="fa fa-search"></span>
+                                                                            <a href="#" class="fa fa-times hide filter-clear"><span class="sr-only">Clear filter</span></a>
+                                                                        </span>
+                                                                        <input type="text" placeholder="Search in the list" id="input-bts-ex-4" class="form-control live-search" aria-describedby="search-icon3" tabindex="1" />
+                                                                    </div>
+                                                                </div>
+                                                                <div class="list-to-filter">
+                                                                    <ul class="list-unstyled">
+                                                                        <?php for ($i=0; $i < count($pegawai) ; $i++) { ?>
+                                                                            <?php if ($pegawai[$i]->idUser!=0) {?>
+                                                                            <li class="filter-item items" data-filter="<?php echo $pegawai[$i]->nama; echo' - '; echo $pegawai[$i]->jabatan?>" data-value="<?php echo $pegawai[$i]->idUser?>"><?php echo $pegawai[$i]->nama; echo' - '; echo $pegawai[$i]->jabatan?></li>
+                                                                            <?php } ?>
+                                                                        <?php } ?>
+                                                                    </ul>
+                                                                    <div class="no-search-results">
+                                                                        <div class="alert alert-warning" role="alert"><i class="fa fa-warning margin-right-sm"></i>No entry for <strong>'<span></span>'</strong> was found.</div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <input type="hidden" name="idUser" value="0" required="">
+                                                    </div>
+                                                    <div class="col-md-12">
+                                                        <div class="hr-line-dashed"></div>
+                                                        <button class="btn btn-sm btn-primary pull-right m-t-n-xs" type="submit"><strong>Lanjutkan</strong></button>
+                                                    </div>
+                                                    <?php echo form_close()?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End -->
                             </div>
                         </div>
                     </div>
