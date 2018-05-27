e4114ee7f1fa0c32687b818c72068798ad5871ca
ferdian05 <ferdian.widyatama14@gmail.com>
2018-05-28 02:47:33 +0700

    update

--- application/views/user/voiceofcustomer.php
+++ application/views/user/voiceofcustomer.php
@@ -72,14 +72,14 @@
                             </div>
                             <div class="row">
                                 <div class="col-md-3">
-                                    <label>Akun</label>
+                                    <label>Tahun</label>
                                     <div class="form-group">
                                         <select name="tahun" class="form-control" required="">
                                             <option value="2018" <?php if ($tahun_pilih==2018){echo " selected";} ?>>2018</option>
                                             <option value="2019" <?php if ($tahun_pilih==2019){echo " selected";} ?> >2019</option>
                                             <option value="2020" <?php if ($tahun_pilih==2020){echo " selected";} ?> >2020</option>
                                             <option value="2021" <?php if ($tahun_pilih==2021){echo " selected";} ?> >2021</option>
-                                            <option value="2022" <?php if ($tahun_pilih==2022){echo " selected";} ?> >2012</option>
+                                            <option value="2022" <?php if ($tahun_pilih==2022){echo " selected";} ?> >2022</option>
                                         </select>
                                     </div>
                                 </div>
